baaa6859180f257c5da3296718810e4538f2caa4
Micah Eschbacher <michael-e@superbrightleds.com>
2014-06-13 15:38:52 -0500

    Expectations for the classy specs

---
+++ expectations/classy.php
@@ -0,0 +1,20 @@
+<?php
+$expected = [];
+
+$expected['class.basic'] = <<<CODE
+class Name
+{
+}
+CODE;
+
+$expected['class.extends'] = <<<CODE
+class Name extends ParentClass
+{
+}
+CODE;
+
+$expected['class.interfaces'] = <<<CODE
+class Name implements Interface1, Interface2, Interface3
+{
+}
+CODE;
