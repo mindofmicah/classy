5d0ab21989b75c1d55ea6a45cc0f0f4b6ce247f9
Micah Eschbacher <michael-e@superbrightleds.com>
2014-06-13 10:14:08 -0500

    Added a custom matcher

--- spec/MindOfMicah/Classy/FunkySpec.php
+++ spec/MindOfMicah/Classy/FunkySpec.php
@@ -3,12 +3,12 @@
 namespace spec\MindOfMicah\Classy;
 
 use Prophecy\Argument;
+use PhpSpec\ObjectBehavior;
 
 class FunkySpec extends ObjectBehavior
 {
     public function let()
     {
-        $this->loadExpections('expectations/funky.php');
         $this->beConstructedWith('name');
     }
     function it_is_initializable()
@@ -18,62 +18,104 @@ class FunkySpec extends ObjectBehavior
 
     public function it_should_render_an_empty_function_with_comments()
     {
-        $this->hasComments()->render()->shouldEqual($this->expect('empty_with_comments'));
+        $this->hasComments()
+            ->render()
+            ->shouldRender('empty_with_comments');
     }
 
     public function it_should_render_an_empty_function()
     {
-        $this->render()->shouldEqual($this->expect('default'));
+        $this->render()
+            ->shouldRender('default');
     }
 
     public function it_should_render_out_each_line_of_the_function()
     {
-        $this->line('$a = "apples"')->line('$a.= " and nanners"')->render()->shouldBe($this->expect('has_lines'));
+        $this->line('$a = "apples"')
+            ->line('$a.= " and nanners"')
+            ->render()
+            ->shouldRender('has_lines');
     }
 
     public function it_should_allow_static_methods()
     {
-        $this->isStatic()->render()->shouldEqual($this->expect('static'));
+        $this->isStatic()
+            ->render()
+            ->shouldRender('static');
     }
     public function it_should_allow_private_methods()
     {
-        $this->isPrivate()->render()->shouldEqual($this->expect('private'));
+        $this->isPrivate()
+            ->render()
+            ->shouldRender('private');
     }
     public function it_should_allow_protected_methods()
     {
-        $this->isProtected()->render()->shouldEqual($this->expect('protected'));
+        $this->isProtected()
+            ->render()
+            ->shouldRender('protected');
     }
 
     public function it_should_be_able_to_go_back_to_being_public()
     {
-        $this->isPrivate()->isPublic()->render()->shouldEqual($this->expect('default'));
+        $this->isPrivate()
+            ->isPublic()
+            ->render()
+            ->shouldRender('default');
     }
     public function it_should_allow_a_syntax_sugar_for_return_statements()
     {
-        $this->line("\$a = 'apples'")->returns('$a')->render()->shouldEqual($this->expect('return_statement'));
+        $this->line("\$a = 'apples'")
+            ->returns('$a')
+            ->render()
+            ->shouldRender('return_statement');
     }
     public function it_should_be_chainable()
     {
-        $this->isChainable()->render()->shouldEqual($this->expect('chainable'));
+        $this->isChainable()
+            ->render()
+            ->shouldRender('chainable');
     }
 
     public function it_should_give_authority_to_chainable_if_it_is_used_with_returns()
     {
-        $this->isChainable()->returns('$a')->render()->shouldEqual($this->expect('chainable'));
+        $this->isChainable()
+            ->returns('$a')
+            ->render()
+            ->shouldRender('chainable');
     }
 
     public function it_should_display_parameters_in_the_declaration()
     {
-        $this->param('$a')->param('$b')->render()->shouldEqual($this->expect('params.basic'));
+        $this->param('$a')
+            ->param('$b')
+            ->render()
+            ->shouldRender('params.basic');
     }
 
     public function it_should_replace_existing_params_with_params_method()
     {
-        $this->param('$c')->params('$a', '$b')->render()->shouldEqual($this->expect('params.basic'));
+        $this->param('$c')
+            ->params('$a', '$b')
+            ->render()
+            ->shouldRender('params.basic');
     }
 
     public function it_should_include_comments_for_parameters()
     {
-        $this->params('\Models\Model $a', '$b')->hasComments()->render()->shouldEqual($this->expect('params.commented'));
+        $this->params('\Models\Model $a', '$b')
+            ->hasComments()
+            ->render()
+            ->shouldRender('params.commented');
+    }
+
+    public function getMatchers()
+    {
+        $expects = require 'expectations/funky.php';
+        return [
+            'render' => function ($subject, $key) use ($expects) {
+                return array_key_exists($key, $expects) && $expects[$key] == $subject;
+            }
+        ];
     }
 }
