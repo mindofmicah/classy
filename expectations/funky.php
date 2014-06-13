8e6603e872eb377b16b9976132739d9ec5776848
Micah Eschbacher <michael-e@superbrightleds.com>
2014-06-13 10:13:18 -0500

    Pulled the expectations into their own file

---
+++ expectations/funky.php
@@ -0,0 +1,83 @@
+<?php
+$expected = [];
+$expected['empty_with_comments'] = <<<CODE
+/**
+ * Description for name
+ *
+ * @return
+ */
+public function name()
+{
+}
+CODE;
+
+$expected['default'] = <<<CODE
+public function name()
+{
+}
+CODE;
+
+$expected['has_lines'] = <<<CODE
+public function name()
+{
+    \$a = "apples";
+    \$a.= " and nanners";
+}
+CODE;
+
+$expected['static'] = <<<CODE
+public static function name()
+{
+}
+CODE;
+
+$expected['private'] = <<<CODE
+private function name()
+{
+}
+CODE;
+
+$expected['protected'] = <<<CODE
+protected function name()
+{
+}
+CODE;
+
+$expected['return_statement'] = <<<CODE
+public function name()
+{
+    \$a = 'apples';
+    return \$a;
+}
+CODE;
+
+$expected['chainable'] = <<<CODE
+public function name()
+{
+    return \$this;
+}
+CODE;
+
+$expected['params.basic'] = <<<CODE
+public function name(\$a, \$b)
+{
+}
+CODE;
+
+$expected['params.commented'] = <<<CODE
+/**
+ * Description for name
+ *
+ * @param \Models\Model \$a
+ * @param string \$b
+ *
+ * @return
+ */
+public function name(\Models\Model \$a, \$b)
+{
+}
+CODE;
+
+// Do Not Remove
+return $expected;
+
